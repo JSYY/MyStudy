1b94df634ff6c8cdc570f23a4e5efa25408e43cd
jiawei.lu <jiawei.lu@united-imaging.com>
2022-08-01 15:25:42 +0800

    增加单例模式

---
+++ DesignPatterns/SinglePattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class SinglePattern
+    {
+        private static object _lock = new object();
+        private static SinglePattern _singlePattern;
+        private SinglePattern()
+        {
+        }
+
+        public SinglePattern GetInstance()
+        {
+            
+            if(_singlePattern == null)
+            {
+                lock (_lock)
+                {
+                    if(_singlePattern == null)
+                    {
+                        return _singlePattern = new SinglePattern();
+                    }
+                }
+            }
+            return _singlePattern;
+        }
+    }
+}
