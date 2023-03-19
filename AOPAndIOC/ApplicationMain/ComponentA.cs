0ecfd7f29bd4460ba15cbefbf94830a44f4facce
JSYY <854073521@qq.com>
2023-03-19 16:04:48 +0800

    多接口实现排序

--- AOPAndIOC/ApplicationMain/ComponentA.cs
+++ AOPAndIOC/ApplicationMain/ComponentA.cs
@@ -1,4 +1,5 @@
-﻿using MyUnity.Attributes;
+﻿using ApplicationMain.ConditionsHandler;
+using MyUnity.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,13 +16,26 @@ namespace ApplicationMain
     [Component]
     public class ComponentA : IComponentA
     {
-        public ComponentA()
+        private List<IConditionHandler> _conditionHandlers;
+        public ComponentA(List<IConditionHandler> conditionHandlers)
         {
+            _conditionHandlers = Sort(conditionHandlers);
+
+        }
+
+        private List<IConditionHandler> Sort(List<IConditionHandler> conditionHandlers)
+        {
+            conditionHandlers.Sort((x,y)=>x.GetExcuteOrder().CompareTo(y.GetExcuteOrder()));
+            return conditionHandlers;
         }
 
         public void FunctionA()
         {
             Console.WriteLine("FunctionA Excute");
+            _conditionHandlers.ForEach(item =>
+            {
+                item.Excute();
+            });
         }
     }
 }
