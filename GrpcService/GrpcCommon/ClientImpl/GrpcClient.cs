b22734dd8c1873d872b54fc62e80a47944c6b70e
jiawei.lu <jiawei.lu@united-imaging.com>
2023-03-20 09:36:14 +0800

    grpc提交

---
+++ GrpcService/GrpcCommon/ClientImpl/GrpcClient.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using gRpcDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcCommon.ClientImpl
+{
+    public class GrpcClient
+    {
+        GrpcService.GrpcServiceClient _clientBase;
+        private string _target;
+        public bool _enable = false;
+
+        public GrpcClient(Address address)
+        {
+            _target = address.IP + ":" + address.Port;
+            CreateClient(_target);
+        }
+
+        private void CreateClient(string target)
+        {
+            if (target == string.Empty)
+                return;
+            var channel = new Channel(target, ChannelCredentials.Insecure);
+            _clientBase = new GrpcService.GrpcServiceClient(channel);
+            _enable = true;
+        }
+
+        public Relay SendMessage(Message message)
+        {
+            var result = _clientBase.SendMessage(message);
+            return result;
+        }
+    }
+}
---
+++ GrpcService/GrpcCommon/GrpcCommunication/GrpcCommunication.cs
@@ -0,0 +1,59 @@
+﻿using GrpcCommon.ClientImpl;
+using GrpcCommon.ServerImpl;
+using gRpcDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcCommon.GrpcCommunication
+{
+    public class GrpcCommunication
+    {
+        private GrpcServer _server;
+        private Dictionary<string, GrpcClient> _clientDic;
+
+        public GrpcCommunication()
+        {
+            _clientDic = new Dictionary<string, GrpcClient>();
+        }
+
+        public void StartServer(Address addressInfo)
+        {
+            _server = new GrpcServer(addressInfo);
+            System.Console.WriteLine("listen on:" + addressInfo.Port);
+        }
+
+        public void AddClient(Address address)
+        {
+            RemoveClient(address.Name);
+            _clientDic.Add(address.Name, new GrpcClient(address));
+        }
+
+        public void RemoveClient(string name)
+        {
+            if (_clientDic.ContainsKey(name))
+                _clientDic.Remove(name);
+        }
+
+        public Relay SendMessage(Message message)
+        {
+
+            message.Sender = _server.GetName();
+            if (_clientDic.ContainsKey(message.Receiver))
+            {
+                return _clientDic[message.Receiver].SendMessage(message);
+            }
+            return null;
+        }
+
+        public int RegisterMessageHandler(int cmd, GrpcMessageHandler handler)
+        {
+            if (handler == null)
+                return 1;
+            _server.RegisterMessageHandler(cmd, handler);
+            return 0;
+        }
+    }
+}
