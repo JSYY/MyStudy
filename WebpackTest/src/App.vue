7d2ce0e3514e007183657a27ac7158c3568965df
jiawei.lu <jiawei.lu@united-imaging.com>
2023-04-18 15:37:32 +0800

    webpack 整体优化

--- WebpackTest/src/App.vue
+++ WebpackTest/src/App.vue
@@ -1,32 +1,35 @@
 <template>
     <div class="main">
-        <div class="test">this is a test</div>
-        <div class="image-test"></div>
+        <h1 style="color:red">{{ name }}</h1>
     </div>
-    
 </template>
 
-<script lang="js">
-    export default {
 
-    }
+<script lang="ts">
+
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        setup() {
+            const name = "123";
+
+            return {
+                name,
+            };
+        },
+    });
 </script>
 
-<style lang="css" scoped>
-    .main{
-        width:100%;
-        height:100%;
-        background-color:antiquewhite;
-        display:flex;
-        flex-direction:column;
+<style lang="css">
+    body{
+        padding:0;
+        margin:0;
     }
-    .test{
-        width:200px;
-        height:200px;
-        background-color:blue;
-    }
-    .image-test{
-        height:1000px;
-        background:url(../public/catImage.jpg);
+    .main {
+        width: 100%;
+        height: 100%;
+        background: url(../public/image/image.jpeg);
+        position: absolute;
+        overflow: hidden;
     }
 </style>
--- WebpackTest/webpack.config.js
+++ WebpackTest/webpack.config.js
@@ -1,45 +1,75 @@
-const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin }  = require("vue-loader");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { DefinePlugin } = require('webpack');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
-
-    },
+    mode:"development",
+    entry: "./src/main.ts",
     module: {
         rules: [
-            { test: /\.vue$/, use: 'vue-loader' },
             {
-                test: /\.css$/, use: [
-                    {
-                        loader:'vue-style-loader'
-                    },
-                    {
-                        loader:'css-loader'
-                    }
-                ]
+                test: /\.vue$/,
+                use: "vue-loader",
             },
             {
-                // 处理图片资源
-                test: /\.(jpg|png|gif)$/,
-                // webpack5中使用assets-module（url-loader已废弃）
-                type: 'asset',
-                parser: {
-                    dataUrlCondition: {
-                        maxSize: 10 * 1024
-                    }
+                test: /\.(t|j)s$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                    },
                 },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                type: "asset",
                 generator: {
-                    filename: 'img/[name].[hash:6][ext]',
-                }
-            }
+                    filename: "images/[name]-[hash][ext]",
+                },
+            },
         ],
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            title: "webpackTest",
+            template: "./index.html",
+            minify: {
+                collapseWhitespace: true, // 去掉空格
+                removeComments: true // 去掉注释
+            }
+        }),
         new VueLoaderPlugin(),
         new CleanWebpackPlugin(),
+        new DefinePlugin({
+                '__VUE_OPTIONS_API__': true,//清除界面上的vue警告
+                 '__VUE_PROD_DEVTOOLS__': false
+        })
     ],
-}
+    target: "web",
+    devServer: {
+        hot: true, // 启用热模块替换
+        open: true, // 打开默认浏览器
+    },
+    //devServer: {
+    //    proxy: {
+    //        "/api": {
+    //            // 需要代理到的真实目标服务器，如/api/user会被代理到https://www.juejin.cn/api/user
+    //            target: "https://www.juejin.cn",
+    //            // 是否更改代理后请求的headers中host地址，某些安全级别较高的服务器会对此做校验
+    //            changeOrigin: true,
+    //            // 默认情况下不接受将请求转发到https的api服务器上，如果希望支持，可以设置为false
+    //            secure: false,
+    //            // 默认情况下/api也会写入到请求url中，通过这个配置可以将其删除
+    //            pathRewrite: {
+    //                "^/api": "/",
+    //            },
+    //        },
+    //    },
+    //},
+};
