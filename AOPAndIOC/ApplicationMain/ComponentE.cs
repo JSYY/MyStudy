f91982a7d813750fec0be25c693c6245183567f4
jiawei.lu <jiawei.lu@united-imaging.com>
2023-07-21 14:47:20 +0800

    文件拷贝进度更新

--- AOPAndIOC/ApplicationMain/ComponentE.cs
+++ AOPAndIOC/ApplicationMain/ComponentE.cs
@@ -22,10 +22,13 @@ namespace ApplicationMain
     public class ComponentE:IComponentE,IDisposable
     {
         private int bufferSize = 1024*1024;
-        private int alreadyTransferData = 0;
-        private int totalSize = 0;
+        private long alreadyTransferData = 0;
+        private long totalSize = 0;
         private bool disposedValue;
         private readonly Timer _timer;
+        private List<string> pathList;
+        private List<string> directoryList;
+        private DateTime startTime;
 
         public ComponentE()
         {
@@ -34,28 +37,94 @@ namespace ApplicationMain
 
         private void CalculateVelocityAndRemainingTime()
         {
-            var v = alreadyTransferData/(1024*1024);
-            Console.WriteLine("current velocity:"+v);
-            var remainingTime = (totalSize - alreadyTransferData / (1024 * 1024)) / v;
-            Console.WriteLine("remainingTime:"+remainingTime);
+            var v = (alreadyTransferData / (1024 * 1024)) / (DateTime.Now - startTime).TotalSeconds;
+            Console.WriteLine("current velocity:"+Math.Round(v,1)+"M/s");
+            if (v != 0)
+            {
+                var remainingTime = ((totalSize - alreadyTransferData )/ (1024 * 1024)) / v;
+                Console.WriteLine("remainingTime:" + sec_to_hms(remainingTime));
+            }
+            var percent= ((float)alreadyTransferData / (1024 * 1024) )/ ((float)totalSize / (1024 * 1024));
+            Console.WriteLine("current process:"+ Math.Round(percent,2)*100+"%");
+        }
+
+        private string sec_to_hms(double duration)
+        {
+            TimeSpan ts = new TimeSpan(0, 0, Convert.ToInt32(duration));
+            string str = "";
+            if (ts.Hours > 0)
+            {
+                str = String.Format("{0:00}", ts.Hours) + ":" + String.Format("{0:00}", ts.Minutes) + ":" + String.Format("{0:00}", ts.Seconds);
+            }
+            if (ts.Hours == 0 && ts.Minutes > 0)
+            {
+                str = "00:" + String.Format("{0:00}", ts.Minutes) + ":" + String.Format("{0:00}", ts.Seconds);
+            }
+            if (ts.Hours == 0 && ts.Minutes == 0)
+            {
+                str = "00:00:" + String.Format("{0:00}", ts.Seconds);
+            }
+            return str;
         }
 
         public void CopyFloder(string sourcePath,string targetPath)
         {
-            totalSize = CalculateTotalSize(sourcePath);
+            //开始初始化文件与文件夹相关信息
+            Init(sourcePath);
             _timer.Change(0, 1000);
+            int count = 1;
+            pathList.ForEach(item =>
+            {
+                CopyFile(item, targetPath+count+"txt");
+                count++;
+            });
+            
+            _timer.Change(Timeout.Infinite, 0);
+        }
+
+        private void CreateFloder(string targetPath)
+        {
             
         }
 
-        private int CalculateTotalSize(string sourcePath)
+        private void Init(string sourcePath)
         {
-            throw new NotImplementedException();
+            pathList = new List<string>();
+            directoryList = new List<string>();
+            alreadyTransferData = 0;
+            totalSize = 0;
+            startTime = DateTime.Now;
+            getDirectory(sourcePath);
+        }
+
+        private void getDirectory(string path)
+        {
+            if (!directoryList.Contains(path))
+            {
+                directoryList.Add(path);
+            }
+            getFileName(path);
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach(DirectoryInfo d in root.GetDirectories())
+            {
+                getDirectory(d.FullName);
+            }
+        }
+
+        private void getFileName(string path)
+        {
+           
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach(FileInfo f in root.GetFiles())
+            {
+                pathList.Add(f.FullName);
+                totalSize += f.Length;
+            }
         }
 
         public void CopyFile(string sourceFile, string targetFile)
         {
             byte[] array = new byte[bufferSize]; //创建缓冲区
-            float percent = 0;
             using (FileStream fsRead = File.Open(sourceFile, FileMode.Open, FileAccess.Read))
             {
                 using (FileStream fsWrite = File.Open(targetFile, FileMode.Create, FileAccess.Write))
@@ -66,10 +135,6 @@ namespace ApplicationMain
                         int length = fsRead.Read(array, 0, array.Length);
                         //从缓冲区写到新文件
                         fsWrite.Write(array, 0, length);
-                        //计算进度
-                        percent = (float)fsRead.Position / totalSize;
-                        //输出进度
-                        Console.WriteLine(percent.ToString("p"));
                         alreadyTransferData += array.Length;
                     }
                   
